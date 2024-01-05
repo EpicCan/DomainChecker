6a32dda8ba5157d46395da64ac035f3ad0722fe4
FastIsSlow <769658249@qq.com>
2024-01-05 13:32:55 +0800

    second commit

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function checkDomain() {
     const fullDomain = document.getElementById('domainInput').value;
     const [name, suffix] = fullDomain.split('.');
 
-    fetch(`/api/?name=${name}&suffix=${suffix}`)
+    fetch(`/api/checkDomain?name=${name}&suffix=${suffix}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data.available);
             document.getElementById('result').innerText = 
                 data.available === true ? '域名未被注册' : '域名已被注册';
         })
