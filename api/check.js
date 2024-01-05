794bcdd82487411fe1bb2a394640e83ad1dc69f8
FastIsSlow <769658249@qq.com>
2024-01-05 14:12:16 +0800

    seven commit

--- api/check.js
+++ api/check.js
@@ -1,11 +1,13 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
+    const fetch = (await import('node-fetch')).default;
     const { domain } = req.query;
     const [name, suffix] = domain.split('.');
 
     try {
         const apiResponse = await fetch(`https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`);
+        if (!apiResponse.ok) {
+            throw new Error('API request failed');
+        }
         const data = await apiResponse.json();
         const message = data.available ? '域名未被注册' : '域名已被注册';
         res.status(200).json({ message });
