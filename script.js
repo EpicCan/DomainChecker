e824389e8441b0fc86807528d118a594c3a5cdb1
FastIsSlow <769658249@qq.com>
2024-01-08 10:59:30 +0800

    add second page

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ function checkDomain() {
     fetch(`/api/check?domain=${domain}`)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('result').innerText = data.message;
+            // document.getElementById('result').innerText = data.message;
+            // 打开新页面并传递结果
+            window.open(`result.html?message=${encodeURIComponent(data.message)}`);
         })
         .catch(error => {
-            document.getElementById('result').innerText = 'Unable to check domain name';
+            // document.getElementById('result').innerText = 'Unable to check domain name';
+            window.open(`result.html?message=${encodeURIComponent('Unable to check domain name')}`);
         });
 }
