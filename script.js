400c5b47496dd938d169575be8ed6c7e944979d3
金灿 <jincan@jincandeMacBook-Air.local>
2024-01-14 12:21:23 +0800

    update windows open way

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ function checkDomain() {
             window.open(`result.html?message=${encodeURIComponent('Unable to check domain name')}`);
         });
 }
+
+
+function checkDomain2() {
+    const domain = document.getElementById('domainInput').value;
+    console.log("Checking domain:", domain); // 添加日志
+
+
+    fetch(`/api/check?domain=${domain}`)
+        .then(response => response.json())
+        .then(data => {
+            // document.getElementById('result').innerText = data.message;
+            console.log("Received data:", data); // 添加日志
+            // 直接在当前页面显示结果
+            document.getElementById('result').innerText = data.message;
+        })
+        .catch(error => {
+            // document.getElementById('result').innerText = 'Unable to check domain name';
+            console.error("Error:", error); // 添加错误日志
+            // 在当前页面显示错误信息
+            document.getElementById('result').innerText = 'Unable to check domain name';
+        });
+}
